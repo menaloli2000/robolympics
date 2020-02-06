ebcf2837210a34fb15cf3a61f0b7092ba82cfd5b
menaloli2000 <52796958+menaloli2000@users.noreply.github.com>
2020-02-06 12:26:17 +0200

    Update no2.ino

--- no2.ino
+++ no2.ino
@@ -1,11 +1,11 @@
 /*loops*/
 
 //if
-    //int i=3;
-    /*if (i>0){
+    int i=3;
+    if (i>0){
         print(i);
         i--;
-    }*/
+    }
 
 //while
     /* while (condition){
@@ -18,14 +18,14 @@
         sttmnts;
     }*/
 
-    /* for(int i =3; i>0;i--){
+     for(int i =3; i>0;i--){
         print(i);
-    }*/ //init>>check cndtn>>sttmnt>>inc/dec>>check cndtn>>sttmnt>> dec/inc
+    } //init>>check cndtn>>sttmnt>>inc/dec>>check cndtn>>sttmnt>> dec/inc
 
 /* break; >>bytl3 barra 2eloop 5ales*/ //law fee loop s8yra gowwa loop kbeera 3andel break hytl3 barra 2els8yra weyed5ol fel kbeera
 /* continue; >>btrg3k le2awwel 2el loop fo2 */
 
-/*int i = -5 
+int i = -5 
 while(i>=5)
 {
     if(i>=0)
@@ -35,7 +35,8 @@ while(i>=5)
         continue;
     }
     Print("aviation");
-} */ //aviation hatteb3 0 times
+}  //aviation hatteb3 0 times
+
 void setup() {
 
 }
