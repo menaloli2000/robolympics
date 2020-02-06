2b0e38a4666f9fcb613383ff7757b5baa5814084
menaloli2000 <52796958+menaloli2000@users.noreply.github.com>
2020-02-06 12:13:16 +0200

    Add files via upload

---
+++ no1.ino
@@ -0,0 +1,18 @@
+/*ksm 7sam*/
+//law 3ayez t48l LED
+//+ve 2el LED hatwassalo be pin 13 wel -ve be pin GND
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(13,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite (13,HIGH); //LAZEM CAPS (HIGH,OUTPUT)
+  //HIGH=5V
+  //KEDA PIN 13 >> O/P 5v
+  //5ALENA HIGH FEL LOOP 34an yfdal yemeddo bekhraba 3latool
+}
+
+/*Delay function: (time in milliseconds)*/
+//DELAY(1000) == 2estanna 1 sec
---
+++ no2.ino
@@ -0,0 +1,25 @@
+/*ksm 7sam*/
+//law 3ayez t4a88al led 1 sec we te2fel 2 sec we tkarrar keda
+void setup() {
+  pinMode(13,OUTPUT); //law hat5alle 2el o/p men 5v, 3.3v m4 ht3rf tt7km feeh bel code
+  
+}
+
+void loop() {  /*law kont 3yzha t3ml kda marra wa7da bas hakteb 2el kalam da fe void setup()*/
+
+  digitalWrite (13,HIGH);
+  DELAY(1000);
+  digitalWrite (13,LOW);
+  DELAY (2000);
+}
+////////////////////////////////
+/* if(<5) {
+    oh yeah
+}
+else if (maybe < 10) {hmmmmmmm......}
+else {can walk, no talk}
+*/
+//////////////////////////////////////
+//Logic operands:
+// (|| OR) (&& AND) (NOT !!)
+//////////////////////////////////////
